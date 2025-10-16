4db280035f58319b3d725b4af8f12abeb2f1d74d
subrata <subrata@ascinatetech.com>
2025-10-16 12:33:28 +0530

    today list page

--- src/app/components/Searchlisting.js
+++ src/app/components/Searchlisting.js
@@ -14,6 +14,13 @@ import FooterSubpage from "./FooterSubpage";
 
 
 export default function Searchlisting() {
+
+   const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggled(prev => !prev); 
+  };
+  
   const searchParams = useSearchParams();
 const searchKeyword = searchParams.get("search");
 
@@ -24,6 +31,7 @@ const searchKeyword = searchParams.get("search");
   const [filters, setFilters] = useState({ categories: [], colors: [], types: [] });
   const [totalIcons, setTotalIcons] = useState(0);
 
+ 
 
   const mianmenu = [
         { id: 1, title: 'Icons' , link: '/icons'  },
@@ -75,13 +83,34 @@ const searchKeyword = searchParams.get("search");
   return (
     <>
       
-    <div className="totals-sections-mains01">
+    <div className={`totals-sections-mains01 ${isToggled ? 'swowpactive' : ''}`}>
         <aside className="sidebars-subpages pe-4 ps-3 pt-3">
-            <Image loading="lazy" src="/icons_guru_logo.svg"
+            <div className="looso-shape d-flex align-items-center">
+              <div className="isons">
+               <Image loading="lazy" src="/shape-icons015.svg"
                                   alt="iconsguru"
-                                  width={160}
+                                  width={33}
                                   height={34} />
+              </div> 
+              <div className="text-loghos">
+                <Image loading="lazy" src="/full-text015.svg"
+                  alt="iconsguru"
+                  width={170}
+                  height={25} />
+                </div>
+            </div>
+            
             <div className="slider-filtersr-div">
+                <button onClick={handleToggle} className="btn w-100 comon_heading01 d-flex align-items-center filters-btn">
+                  <span className="icpn-svg">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(0,0,0,1)">
+                      <path d="M5 7C5 6.17157 5.67157 5.5 6.5 5.5C7.32843 5.5 8 6.17157 8 7C8 7.82843 7.32843 8.5 6.5 8.5C5.67157 8.5 5 7.82843 5 7ZM6.5 3.5C4.567 3.5 3 5.067 3 7C3 8.933 4.567 10.5 6.5 10.5C8.433 10.5 10 8.933 10 7C10 5.067 8.433 3.5 6.5 3.5ZM12 8H20V6H12V8ZM16 17C16 16.1716 16.6716 15.5 17.5 15.5C18.3284 15.5 19 16.1716 19 17C19 17.8284 18.3284 18.5 17.5 18.5C16.6716 18.5 16 17.8284 16 17ZM17.5 13.5C15.567 13.5 14 15.067 14 17C14 18.933 15.567 20.5 17.5 20.5C19.433 20.5 21 18.933 21 17C21 15.067 19.433 13.5 17.5 13.5ZM4 16V18H12V16H4Z"></path>
+                    </svg>
+                  </span>
+                  <h4 className="ms-2"> Filter <span className='arose'> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                  </svg>  </span> </h4>
+               </button>
               <SidebarFilter onFilterChange={setFilters}  showCategoryFilter={false} />
             </div>
         </aside>
